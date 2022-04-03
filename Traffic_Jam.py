96a8b43eebbfa40dfda2d917431af63f06be09e4
Patrick Mauger <38781639+Patochun@users.noreply.github.com>
2022-04-03 19:24:02 +0200

    Update Traffic_Jam.py

--- Traffic_Jam.py
+++ Traffic_Jam.py
@@ -6,9 +6,9 @@ import random
 # ********************************************************************
 # Traffic Jam Simulator
 # Blender version = 2.8
-# Author = Doc OuatZat (DOZ) and by the way is creator (Patrick M)
-# Web Site = docouatzat.com
-# Mail = docouatzat@gmail.com
+# Author = Patochun (Patrick M)
+# Web Site = patochun.wordpress.com
+# Mail = ptkmgr@gmail.com
 #
 # Licence used = Creative Commons CC BY
 # Check licence here : https://creativecommons.org
