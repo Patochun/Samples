b9a5f9c3b208d58cca6fdd4afdda578595e87d14
Patrick Mauger <38781639+Patochun@users.noreply.github.com>
2024-07-17 08:15:43 +0200

    Compatibilty Belnder 4.2 Tested

--- Sieve_of_Erathostene.py
+++ Sieve_of_Erathostene.py
@@ -6,7 +6,7 @@ import random
 
 # ********************************************************************
 # Sieve_of_Erathostene
-# Blender version = 2.8 to 3.5
+# Blender version = 2.8 to 4.2
 # Author = Patochun (Patrick M)
 # Web Site = patochun.wordpress.com
 # Mail = ptkmgr@gmail.com
--- X_Modulo_2D.py
+++ X_Modulo_2D.py
@@ -6,7 +6,7 @@ import random
 
 # ********************************************************************
 # X_Modulo_2D
-# Blender version = 2.8 to 3.5
+# Blender version = 2.8 to 4.2
 # Author = Patochun (Patrick M)
 # Web Site = patochun.wordpress.com
 # Mail = ptkmgr@gmail.com
