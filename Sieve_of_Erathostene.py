8fdf3210290b37d1258a22c24d4f0b855353c665
Patrick Mauger <38781639+Patochun@users.noreply.github.com>
2024-07-21 12:20:19 +0200

    Update Sieve_of_Erathostene.py
    Speedup and simplify

--- Sieve_of_Erathostene.py
+++ Sieve_of_Erathostene.py
@@ -1,8 +1,6 @@
 import bpy
 from bpy_extras.node_shader_utils import PrincipledBSDFWrapper
 import numpy as np
-import math
-import random
 
 # ********************************************************************
 # Sieve_of_Erathostene
@@ -16,14 +14,16 @@ import random
 #
 # Generate animation of Sieve of Erathostene
 # Master variables :
-#   BigCubeEdge = Mean the number of cubes by edge of the big cube.
+#   bigCubeEdge = Mean the number of cubes by edge of the big cube.
 #                 4 is the default value. Feel free to choose yours.
 #                 but keep in mind the consumed ressource and time.
+#
+# Notes : The frame rate chosen for the animation is 25 frames per second.
 # ********************************************************************
 
-BigCubeEdge = 4
-CubesCount = BigCubeEdge ** 3
-ArrayOfCube = np.zeros((CubesCount + 2), dtype=int)
+bigCubeEdge = 5
+cubesCount = bigCubeEdge ** 3
+arrayOfCube = np.zeros((cubesCount + 2), dtype=int)
 
 
 # Research about a collection
@@ -36,32 +36,35 @@ def find_collection(context, item):
 
 # Create collection
 def create_collection(collection_name, parent_collection):
+    # delete if exist
     if collection_name in bpy.data.collections:
-        return bpy.data.collections[collection_name]
-    else:
-        new_collection = bpy.data.collections.new(collection_name)
-        parent_collection.children.link(new_collection)
-        return new_collection
+        collection = bpy.data.collections[collection_name]
+        for ob in collection.objects:
+            bpy.data.objects.remove(ob, do_unlink=True)
+        bpy.data.collections.remove(collection)
+    # create a new one
+    new_collection = bpy.data.collections.new(collection_name)
+    parent_collection.children.link(new_collection)
+    return new_collection
 
 
 # Create a text mesh
-def Add_Text(collect, Name, x, y, z, Texte, mat1, mat2, mat3):
+def add_text(collect, name, mat1, mat2):
     bpy.ops.object.text_add()
     ot = bpy.context.active_object
-    ot.name = 't_' + Name
-    ot.data.body = Texte
+    ot.name = 't_' + name
+    ot.data.body = name
     ot.data.extrude = 0.1
-    ot.location.x = (x * 2.5) - (len(Texte)/4)
-    ot.location.y = (y * 2.5) - 0.4
-    ot.location.z = (z * 2.5) + 1.0
+    ot.location.x = -(len(name)/4)
+    ot.location.y = -0.4
+    ot.location.z = 1.0
 
     o = bpy.context.object
     bpy.ops.object.convert(target='MESH', keep_original=False)
 
     # assign to material slots - Both cube and text for union later
-    o.data.materials.append(mat1)
-    o.data.materials.append(mat2)
-    o.data.materials.append(mat3)
+    o.data.materials.append(mat1) # index 0
+    o.data.materials.append(mat2) # index 1
 
     collect_to_unlink = find_collection(bpy.context, o)
     collect.objects.link(o)
@@ -71,10 +74,10 @@ def Add_Text(collect, Name, x, y, z, Texte, mat1, mat2, mat3):
 
 
 # Create a Cube Mesh
-def Add_Cube(collect, Name, x, y, z, mat1, mat2, mat3):
+def add_cube(collect, name, x, y, z, mat1):
     bpy.ops.mesh.primitive_cube_add()
     oc = bpy.context.active_object
-    oc.name = 'c_' + Name
+    oc.name = 'c_' + name
     oc.location.x = x * 2.5
     oc.location.y = y * 2.5
     oc.location.z = z * 2.5
@@ -83,12 +86,10 @@ def Add_Cube(collect, Name, x, y, z, mat1, mat2, mat3):
 
     # assign to material slots - Both cube and text for union later
     o.data.materials.append(mat1)
-    o.data.materials.append(mat2)
-    o.data.materials.append(mat3)
 
     # Little bevel is always a nice idea for a cube
-    bpy.ops.object.modifier_add(type="BEVEL")
-    bpy.ops.object.modifier_apply(modifier="BEVEL")
+#    bpy.ops.object.modifier_add(type="BEVEL")
+#    bpy.ops.object.modifier_apply(modifier="BEVEL")
 
     collect_to_unlink = find_collection(bpy.context, o)
     collect.objects.link(o)
@@ -96,25 +97,12 @@ def Add_Cube(collect, Name, x, y, z, mat1, mat2, mat3):
 
     return oc
 
-
-# Boolean operation between two objects
-# Type =  [INTERSECT, UNION, DIFFERENCE]
-def applyBoolean(obj_A, obj_B, Type):
-    boo = obj_A.modifiers.new(type='BOOLEAN', name="booh")
-    boo.object = obj_B
-    boo.operation = Type
-    bpy.ops.object.modifier_apply(modifier="booh")
-    bpy.ops.object.select_all(action='DESELECT')
-    bpy.data.objects[obj_B.name].select_set(True)
-    bpy.ops.object.delete()
-
-
 # ------------------
 # Change cube scale
 # ------------------
-def EditScale(scn, n, sc):
-    Obj_Name = 'c_' + str(n)
-    obj = bpy.data.objects[Obj_Name]
+def edit_scale(scn, n, sc):
+    objName = 'c_' + str(n)
+    obj = bpy.data.objects[objName]
     a = scn.frame_current
     obj.keyframe_insert('scale')
     scn.frame_current += 10
@@ -126,9 +114,9 @@ def EditScale(scn, n, sc):
 # ------------------
 # Highlight cube text
 # ------------------
-def Highlight_Cube(scn, n):
-    Obj_Name = 'c_' + str(n)
-    obj = bpy.data.objects[Obj_Name]
+def highlight_cube(scn, n):
+    objName = 't_' + str(n)
+    obj = bpy.data.objects[objName]
 
     a = scn.frame_current
     # Mem state at pos-1 to avoid changing in middle latter
@@ -140,7 +128,7 @@ def Highlight_Cube(scn, n):
     # Change lighting of text by changing material_index
     scn.frame_current += 1
     for f in mesh.polygons:  # iterate over faces
-        if (f.material_index == 2):
+        if (f.material_index == 0):
             f.material_index = 1
             f.keyframe_insert('material_index')
     scn.frame_current = a
@@ -156,41 +144,42 @@ nCol = create_collection("Big_Cube", bpy.context.scene.collection)
 
 # Create materials for cubes
 # Cube body
-mat_c = bpy.data.materials.new(name="Material_Cube")
-mat_c.use_nodes = True
-principled = PrincipledBSDFWrapper(mat_c, is_readonly=False)
+matCube = bpy.data.materials.new(name="matCube")
+matCube.use_nodes = True
+principled = PrincipledBSDFWrapper(matCube, is_readonly=False)
 principled.base_color = (0.8, 0.4, 0.2)
 
 # Highlight Text
-mat_ch = bpy.data.materials.new(name="Material_Highlight")
-mat_ch.use_nodes = True
-principled = PrincipledBSDFWrapper(mat_ch, is_readonly=False)
+matTextHighLight = bpy.data.materials.new(name="matTextHighlight")
+matTextHighLight.use_nodes = True
+principled = PrincipledBSDFWrapper(matTextHighLight, is_readonly=False)
 principled.base_color = (0.4, 1.0, 0.2)
 
 # Normal Text
-mat_t = bpy.data.materials.new(name="Material_Highlight")
-mat_t.use_nodes = True
-principled = PrincipledBSDFWrapper(mat_t, is_readonly=False)
+matTextNormal = bpy.data.materials.new(name="matTextNormal")
+matTextNormal.use_nodes = True
+principled = PrincipledBSDFWrapper(matTextNormal, is_readonly=False)
 principled.base_color = (1.0, 0.0, 0.0)
 
 # Create the 3D array of cube
 k = 0
-for z in range(BigCubeEdge, 0, -1):
-    for y in range(BigCubeEdge, 0, -1):
-        for x in range(0, BigCubeEdge):
+for z in range(bigCubeEdge, 0, -1):
+    for y in range(bigCubeEdge, 0, -1):
+        for x in range(0, bigCubeEdge):
             k += 1
-            ot = Add_Text(nCol, str(k), x, y, z, str(k), mat_t, mat_c, mat_ch)
-            oc = Add_Cube(nCol, str(k), x, y, z, mat_c, mat_ch, mat_t)
-            applyBoolean(oc, ot, "UNION")
+            kString = str(k)
+            ot = add_text(nCol, kString, matTextNormal, matTextHighLight)
+            oc = add_cube(nCol, kString, x, y, z, matCube)
+            ot.parent = oc
 
 # Set animation start
 scn = bpy.context.scene
 scn.frame_current = 1
 
 # Freeze all objects in start state
-for num in range(1, CubesCount):
-    Obj_Name = 'c_' + str(num)
-    obj = bpy.data.objects[Obj_Name]
+for num in range(1, cubesCount):
+    objName = 'c_' + str(num)
+    obj = bpy.data.objects[objName]
     obj.scale = 1.0, 1.0, 1.0
     obj.keyframe_insert('scale')
 
@@ -198,47 +187,47 @@ for num in range(1, CubesCount):
     scn.frame_current = 100
 
 # Freeze all objects in start state (again)
-for num in range(1, CubesCount):
-    Obj_Name = 'c_' + str(num)
-    obj = bpy.data.objects[Obj_Name]
+for num in range(1, cubesCount):
+    objName = 'c_' + str(num)
+    obj = bpy.data.objects[objName]
     obj.scale = 1.0, 1.0, 1.0
     obj.keyframe_insert('scale')
 
 # Play the Sieve of Erathostene
-ArrayOfCube.fill(1)
-ArrayOfCube[0] = 0  # Do not use the 0
-ArrayOfCube[CubesCount+1] = 0  # Mark the last+1
+arrayOfCube.fill(1)
+arrayOfCube[0] = 0  # Do not use the 0
+arrayOfCube[cubesCount+1] = 0  # Mark the last+1
 
 # Main loop of Sieve
-for i in range(1, CubesCount, 1):
+for i in range(1, cubesCount, 1):
 
     # Do not treat the one
     if (i == 1):
-        ArrayOfCube[i] = 0
-        EditScale(scn, i, 0.0)
+        arrayOfCube[i] = 0
+        edit_scale(scn, i, 0.0)
         continue
 
     # Do not treat if already treated
-    if (ArrayOfCube[i] == 0):
+    if (arrayOfCube[i] == 0):
         continue
 
     # Highlight running number
     scn.frame_current += 2
-    Highlight_Cube(scn, i)
-    EditScale(scn, i, 2.0)
+    highlight_cube(scn, i)
+    edit_scale(scn, i, 2.0)
 
     # Sieve all N since 2xN
-    for j in range(i*2, CubesCount+1, i):
+    for j in range(i*2, cubesCount+1, i):
         # Do not treat if already treated (also here)
-        if (ArrayOfCube[j] == 0):
+        if (arrayOfCube[j] == 0):
             continue
-        ArrayOfCube[j] = 0
+        arrayOfCube[j] = 0
         scn.frame_current += 2
-        EditScale(scn, j, 0.0)
+        edit_scale(scn, j, 0.0)
 
     # Downlight running number
     scn.frame_current += 2
-    EditScale(scn, i, 1.0)
+    edit_scale(scn, i, 1.0)
 
 # Leave 4 seconds after Sieve
 scn.frame_end = scn.frame_current + 100
